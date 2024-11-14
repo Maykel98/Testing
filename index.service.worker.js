de97ebbaa28d2f0443dd6a31e4b04374759cf687
Miguelmac <miguelmac@live.com>
2024-11-14 10:28:43 -0300

    Add files via upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,8 +3,8 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1731450702|6731240626";
-const CACHE_PREFIX = "Test block slide-sw-cache-";
+const CACHE_VERSION = "1731590693|753956875";
+const CACHE_PREFIX = "Block slider-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
 // Files that will be cached on load.
